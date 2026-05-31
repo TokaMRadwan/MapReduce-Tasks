7ebd46ccb06b0155d9fb988f9c36bd7d670a036b
Toka Radwan <76154927+TokaMRadwan@users.noreply.github.com>
2026-06-01 01:38:09 +0300

    Add files via upload

---
+++ shakespeare/reducer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from operator import itemgetter
+import sys
+# maps words to their counts
+word2count = {}
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # parse the input we got from mapper.py
+    word, count = line.split('\t', 1)
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+        word2count[word] = word2count.get(word, 0) + count
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        pass
+
+sorted_word2count = sorted(word2count.items(), key=itemgetter(0))
+# write the results to STDOUT (standard output)
+for word, count in sorted_word2count:
+    print ('%s\t%s'% (word, count))
+	
+
